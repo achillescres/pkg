c60c96557c2f8890925f5d92256c1ea801a497c8
achillescres <achillescres@gmail.com>
2023-10-05 23:05:18 +0300

    add ctx to Goards

--- mysync/goard.go
+++ mysync/goard.go
@@ -1,6 +1,7 @@
 package mysync
 
 import (
+	"context"
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"runtime"
@@ -13,44 +14,47 @@ func ChangeLogger(newLogger *logrus.Entry) {
 	logger = newLogger
 }
 
-func Goard(f func()) {
+func Goard(ctx context.Context, f func(context.Context)) {
 	go func() {
 		defer func() {
 			recover()
 		}()
-		f()
+		f(ctx)
 	}()
 }
 
-func GoardWithLog(f func()) {
+func GoardWithLog(ctx context.Context, f func(context.Context)) {
 	go func() {
-		//defer func() {
-		//	if r := recover(); logger != nil && r != nil {
-		//		buf := make([]byte, 64<<10)
-		//		buf = buf[:runtime.Stack(buf, false)]
-		//		err := fmt.Errorf("errgroup: panic recovered: %s\n%s", r, buf)
-		//		logger.Errorln(err)
-		//	}
-		//}()
-		f()
+		defer func() {
+			if r := recover(); logger != nil && r != nil {
+				buf := make([]byte, 64<<10)
+				buf = buf[:runtime.Stack(buf, false)]
+				err := fmt.Errorf("errgroup: panic recovered: %s\n%s", r, buf)
+				logger.Errorln(err)
+			}
+		}()
+		f(ctx)
 	}()
 }
 
-func GoardWithLogger(f func(), logger *logrus.Logger) {
+func GoardWithLogger(ctx context.Context, f func(context.Context), logger *logrus.Logger) {
+	if logger == nil {
+		panic("logger is nil")
+	}
 	go func() {
-		//defer func() {
-		//	if r := recover(); logger != nil && r != nil {
-		//		buf := make([]byte, 64<<10)
-		//		buf = buf[:runtime.Stack(buf, false)]
-		//		err := fmt.Errorf("errgroup: panic recovered: %s\n%s", r, buf)
-		//		logger.Errorln(err)
-		//	}
-		//}()
-		f()
+		defer func() {
+			if r := recover(); logger != nil && r != nil {
+				buf := make([]byte, 64<<10)
+				buf = buf[:runtime.Stack(buf, false)]
+				err := fmt.Errorf("errgroup: panic recovered: %s\n%s", r, buf)
+				logger.Errorln(err)
+			}
+		}()
+		f(ctx)
 	}()
 }
 
-func GoardWithChan(f func(), errChan chan error) {
+func GoardWithChan(ctx context.Context, f func(context.Context), errChan chan error) {
 	if errChan == nil {
 		panic("errChan is nil")
 	}
@@ -63,11 +67,11 @@ func GoardWithChan(f func(), errChan chan error) {
 				errChan <- err
 			}
 		}()
-		f()
+		f(ctx)
 	}()
 }
 
-func GoardWithCallback(f func(), errCallback func(err error)) {
+func GoardWithCallback(ctx context.Context, f func(context.Context), errCallback func(err error)) {
 	if errCallback == nil {
 		panic("errCallback is nil")
 	}
@@ -82,6 +86,6 @@ func GoardWithCallback(f func(), errCallback func(err error)) {
 				}()
 			}
 		}()
-		f()
+		f(ctx)
 	}()
 }
