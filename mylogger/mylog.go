b734e15af780b0cdf5830820d03f86c2d7d34a8b
achillescres <achillescres@gmail.com>
2023-10-20 14:06:46 +0300

    add field sort

--- mylogger/mylog.go
+++ mylogger/mylog.go
@@ -1,11 +1,15 @@
 package mylogging
 
 import (
+	customLog "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
 	"os"
 )
 
 func ConfigureLogrus() {
+	logrus.SetFormatter(&customLog.Formatter{
+		FieldsOrder: []string{"component", "category"},
+	})
 	logrus.SetReportCaller(true)
 	logrus.SetOutput(os.Stdout)
 	logrus.SetLevel(logrus.TraceLevel)
@@ -13,6 +17,9 @@ func ConfigureLogrus() {
 
 func GetLogger() *logrus.Logger {
 	logger := logrus.New()
+	logger.SetFormatter(&customLog.Formatter{
+		FieldsOrder: []string{"component", "category"},
+	})
 	logger.SetReportCaller(true)
 	logger.SetOutput(os.Stdout)
 	logger.SetLevel(logrus.TraceLevel)
