245adda5521adf987bc3907c14abbea494a52feb
achillescres <achillescres@gmail.com>
2023-12-09 04:44:47 +0300

    fix bytes method add error

--- kafkapubsub/message.go
+++ kafkapubsub/message.go
@@ -3,7 +3,7 @@ package kafkapubsub
 // Message is interface for kafka topics use
 type Message interface {
 	// Bytes converts Message to []byte
-	Bytes() []byte
+	Bytes() ([]byte, error)
 	// Scan scans []byte and unmarshals it to Message
 	Scan([]byte) error
 }
