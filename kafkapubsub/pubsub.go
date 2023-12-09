51c3b60bdb3158604f546a47aa107d989263c37f
achillescres <achillescres@gmail.com>
2023-12-09 04:32:47 +0300

    add warning comment

--- kafkapubsub/pubsub.go
+++ kafkapubsub/pubsub.go
@@ -3,6 +3,7 @@ package kafkapubsub
 // Topic is kafka publish and subscribe topic implementation.
 // It uses kafka.Writer and kafka.Reader from kafka-go
 // Specify your own MessageType with Message interface
+// WARNING: barely useless and maybe harmful for you application logic
 type Topic[MessageType Message] struct {
 	PubTopic[MessageType]
 	SubTopic[MessageType]
