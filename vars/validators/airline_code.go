24704adf43f4fb9567a5c44d10ce8ba0fa531346
achillescres <achillescres@gmail.com>
2024-04-17 20:05:27 +0300

    make standart validators bound to ^ and $ | add unbound validators

--- vars/validators/airline_code.go
+++ vars/validators/airline_code.go
@@ -2,6 +2,9 @@ package validators
 
 import "regexp"
 
-var AirlineCodeMatcher = regexp.MustCompile(`^[A-Z0-9]{2}[A-Z0-9]?$`)
-
-var IataMatcher = regexp.MustCompile(`[A-Z]{3}`)
+var (
+	AirlineCodeMatcher          = regexp.MustCompile(`^[A-Z0-9]{2}[A-Z0-9]?$`)
+	UnboundedAirlineCodeMatcher = regexp.MustCompile(`[A-Z0-9]{2}[A-Z0-9]?`)
+	IataMatcher                 = regexp.MustCompile(`^[A-Z]{3}$`)
+	UnboundedIataMatcher        = regexp.MustCompile(`[A-Z]{3}`)
+)
