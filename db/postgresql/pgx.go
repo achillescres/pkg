71bbb9ce65b5aac6a8e872cfa8eb0888f71aac22
achillescres <achillescres@gmail.com>
2025-03-24 01:05:25 +0300

    refactor stdlib

--- db/postgresql/pgx.go
+++ db/postgresql/pgx.go
@@ -7,35 +7,44 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/achillescres/pkg/utils"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/stdlib"
 )
 
-func NewStdlibDB(ctx context.Context, cc ClientConfig) (*sql.DB, error) {
+type StdlibConfig struct {
+	DSN             string
+	UseCA           bool
+	CaAbsPath       string
+	WaitingDuration time.Duration
+}
+
+func NewStdlibDB(ctx context.Context, config StdlibConfig) (*sql.DB, error) {
 	ew := utils.NewErrorWrapper("NewStdlibDB")
 
-	config, err := NewConfigFromClientConfig(&cc)
+	pgxConfig, err := pgx.ParseConfig(config.DSN)
 	if err != nil {
-		return nil, ew(fmt.Errorf("create config: %w", err))
+		return nil, ew(fmt.Errorf("parse config: %w", err))
 	}
 
-	if cc.UseCA {
+	if config.UseCA {
 		rootCertPool := x509.NewCertPool()
-		pem, err := os.ReadFile(cc.CaAbsPath)
+		pem, err := os.ReadFile(config.CaAbsPath)
 		if err != nil {
 			return nil, ew(fmt.Errorf("read ca file: %w", err))
 		}
 		if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
 			return nil, ew(fmt.Errorf("append certs to cert pool: %w", err))
 		}
-		config.ConnConfig.TLSConfig = &tls.Config{
+		pgxConfig.TLSConfig = &tls.Config{
 			RootCAs:            rootCertPool,
 			InsecureSkipVerify: true,
 		}
 	}
 
-	db := stdlib.OpenDB(*config.ConnConfig)
+	db := stdlib.OpenDB(*pgxConfig)
 	if err != nil {
 		return nil, ew(fmt.Errorf("open db: %w", err))
 	}
@@ -43,7 +52,7 @@ func NewStdlibDB(ctx context.Context, cc ClientConfig) (*sql.DB, error) {
 		return nil, ew(fmt.Errorf("error couldn't connect to db"))
 	}
 
-	pingCtx, cancel := context.WithTimeout(ctx, cc.WaitingDuration)
+	pingCtx, cancel := context.WithTimeout(ctx, config.WaitingDuration)
 	defer cancel()
 
 	err = db.PingContext(pingCtx)
