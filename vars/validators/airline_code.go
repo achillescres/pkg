4b4853e625cbf7169ac44cc468c5a5e193238854
achillescres <achillescres@gmail.com>
2025-10-20 00:48:21 +0300

    add crtontab validator

--- vars/validators/airline_code.go
+++ vars/validators/airline_code.go
@@ -7,4 +7,5 @@ var (
 	UnboundedAirlineCodeMatcher = regexp.MustCompile(`[A-Z0-9]{2}[A-Z0-9]?`)
 	IataMatcher                 = regexp.MustCompile(`^[A-Z]{3}$`)
 	UnboundedIataMatcher        = regexp.MustCompile(`[A-Z]{3}`)
+	CrontabMatcher              = regexp.MustCompile("^(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+(\\/|-)\\d+)|\\d+|\\*) ?){5,7})$")
 )
