628a3c44c5a2e5e87c7e25ae69e90c679fbea07f
achillescres <achillescres@gmail.com>
2025-10-20 01:35:25 +0300

    fix crontab

--- vars/validators/airline_code.go
+++ vars/validators/airline_code.go
@@ -7,5 +7,5 @@ var (
 	UnboundedAirlineCodeMatcher = regexp.MustCompile(`[A-Z0-9]{2}[A-Z0-9]?`)
 	IataMatcher                 = regexp.MustCompile(`^[A-Z]{3}$`)
 	UnboundedIataMatcher        = regexp.MustCompile(`[A-Z]{3}`)
-	CrontabMatcher              = regexp.MustCompile(`(CRON_TZ=(UTC|[A-Za-z]+\/[A-Za-z]+) )?((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})`)
+	CrontabMatcher              = regexp.MustCompile(`((CRON_TZ|TZ)=(UTC|[A-Za-z]+\/[A-Za-z]+) )?((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})`)
 )
